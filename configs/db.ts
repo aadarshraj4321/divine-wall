5886a3f5bfef33ca9ac7e338b2abb1a9ac3e5d20
aadarshraj4321 <aadarshraj4321@gmail.com>
2024-12-16 23:12:21 +0530

    database

--- configs/db.ts
+++ configs/db.ts
@@ -1,5 +1,5 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 
-const sql = neon(process.env.NEXT_PUBLIC_NEON_DATABASE as string);  
+const sql = neon("postgresql://neondb_owner:2jmWgiKet1Zh@ep-sparkling-wildflower-a4owzmyt.us-east-1.aws.neon.tech/divine-wall?sslmode=require");  
 export const db = drizzle({ client: sql });
