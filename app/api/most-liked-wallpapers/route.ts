65710ffc6fdf013cc5a27979717d93f3071b2884
aadarshraj4321 <aadarshraj4321@gmail.com>
2024-12-20 21:11:51 +0530

    fixing some bugs in liked wallpapers

--- app/api/most-liked-wallpapers/route.ts
+++ app/api/most-liked-wallpapers/route.ts
@@ -5,17 +5,19 @@ import { sql } from 'drizzle-orm'; // Import sql helper
 
 // Export the GET method for fetching most liked wallpapers
 export async function GET(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    // Querying for the most liked wallpapers with Drizzle ORM
-    const wallpapersData = await db
-      .select()
-      .from(wallpapers)
-      .orderBy(wallpapers.heart_likes, sql`DESC`)
-      .limit(10); // Limit the results to 10 wallpapers
+  // try {
+  //   // Querying for the most liked wallpapers with Drizzle ORM
+  //   const wallpapersData = await db
+  //     .select()
+  //     .from(wallpapers)
+  //     .orderBy(wallpapers.heart_likes, sql`DESC`)
+  //     .limit(10); // Limit the results to 10 wallpapers
 
-    return res.status(200).json(wallpapersData); // Return the results
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: 'Something went wrong' });
-  }
+  //   return res.status(200).json(wallpapersData); // Return the results
+  // } catch (error) {
+  //   console.error(error);
+  //   return res.status(500).json({ message: 'Something went wrong' });
+  // }
+
+  console.log(req);
 }
